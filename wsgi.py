8187b0da861f773c7d7a2897c1693a6e1e71deb3
unknown <raia.vincent@gmail.com>
2015-10-06 23:36:26 -0400

    delete

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-web: gunicorn VincentRaia.wsgi
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial_blog_ng.settings")
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
