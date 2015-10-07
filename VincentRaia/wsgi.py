52e83a7e7c004cc211f9c0c8c5a4448acf98b464
unknown <raia.vincent@gmail.com>
2015-10-07 00:52:39 -0400

    wsgi

--- VincentRaia/wsgi.py
+++ VincentRaia/wsgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VincentRaia.settings")
-
 from dj_static import Cling
-#application = get_wsgi_application()
 application = Cling(get_wsgi_application())
